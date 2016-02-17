347fc1927c55941a030d9badf85c6f778b5b627e
cibernox <miguel.camba@gmail.com>
2016-02-17 10:36:04 +0000

    Prepare css compilation

---
+++ config/release.js
@@ -0,0 +1,24 @@
+/* jshint node:true */
+
+// For details on each option run `ember help release`
+var simpleGit = require('simple-git')();
+
+module.exports = {
+  // local: true,
+  // remote: 'some_remote',
+  // annotation: "Release %@",
+  // message: "Bumped version to %@",
+  // manifest: [ 'package.json', 'bower.json', 'someconfig.json' ],
+  // strategy: 'date',
+  // format: 'YYYY-MM-DD',
+  // timezone: 'America/Los_Angeles',
+  //
+  beforeCommit: function(project, versions) {
+    require('../compile-css.js'); // Requiring the file compiles
+    return new Promise(function(resolve) {
+      simpleGit.add(['vendor/ember-basic-dropdown.css'], function() {
+        resolve();
+      });
+    });
+  }
+};
