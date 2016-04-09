5e6366716429ba856c57291881b057ee833c523e
cibernox <miguel.camba@gmail.com>
2016-04-09 22:47:10 +0100

    Simplified animation logic.
    Less round-loops, less requestAnimationFrame.

--- addon/components/basic-dropdown/content.js
+++ addon/components/basic-dropdown/content.js
@@ -30,6 +30,7 @@ function waitForAnimations(element, callback) {
 
 export default WormholeComponent.extend({
   layout,
+  transitionClass: 'ember-basic-dropdown--transitioning-in',
 
   // Lifecycle hooks
   didInsertElement() {
@@ -44,51 +45,32 @@ export default WormholeComponent.extend({
   willDestroyElement() {
     this._super(...arguments);
     let dropdown = self.window.document.getElementById(this.get('dropdownId'));
-    if (!this.get('renderInPlace')) {
-      this._unsubscribeToFocusEvents(dropdown);
-    }
     this._animateOut(dropdown);
   },
 
   // Methods
   _animateIn(dropdown) {
-    run.schedule('afterRender', this, function() {
-      dropdown.classList.add('ember-basic-dropdown--transitioning-in');
-      rAF(function() {
-        waitForAnimations(dropdown, function() {
-          dropdown.classList.remove('ember-basic-dropdown--transitioning-in');
-          dropdown.classList.add('ember-basic-dropdown--transitioned-in');
-        });
-      });
+    waitForAnimations(dropdown, () => {
+      this.set('animationClass', 'ember-basic-dropdown--transitioned-in');
     });
   },
 
   _animateOut(dropdown) {
+    let parentElement = this.get('renderInPlace') ? dropdown.parentElement.parentElement : dropdown.parentElement;
     let clone = dropdown.cloneNode(true);
-    let parentElement = dropdown.parentElement;
-    if (this.get('renderInPlace')) {
-      parentElement = parentElement.parentElement;
-    }
     clone.id = clone.id + '--clone';
-    run.schedule('afterRender', function() {
-      parentElement.appendChild(clone);
-      rAF(function() {
-        clone.classList.remove('ember-basic-dropdown--transitioned-in');
-        clone.classList.remove('ember-basic-dropdown--transitioning-in');
-        clone.classList.add('ember-basic-dropdown--transitioning-out');
-        waitForAnimations(clone, function() {
-          parentElement.removeChild(clone);
-        });
+    parentElement.appendChild(clone);
+    rAF(function() {
+      clone.classList.remove('ember-basic-dropdown--transitioned-in');
+      clone.classList.remove('ember-basic-dropdown--transitioning-in');
+      clone.classList.add('ember-basic-dropdown--transitioning-out');
+      waitForAnimations(clone, function() {
+        parentElement.removeChild(clone);
       });
     });
   },
 
   _subscribeToFocusEvents(dropdown) {
-    dropdown.addEventListener('focusin', this.get('onFocusIn'));
-    dropdown.addEventListener('focusout', this.get('onFocusOut'));
-  },
-
-  _unsubscribeToFocusEvents(dropdown) {
     dropdown.addEventListener('focusin', this.get('onFocusIn'));
     dropdown.addEventListener('focusout', this.get('onFocusOut'));
   }
