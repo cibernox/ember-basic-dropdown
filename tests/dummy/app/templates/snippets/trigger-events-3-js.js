c0092894b9098ffcd3862a198b2958babee3ef7e
cibernox <miguel.camba@gmail.com>
2017-01-13 23:18:09 +0000

    I think examples are going well

--- tests/dummy/app/templates/snippets/trigger-events-3-js.js
+++ tests/dummy/app/templates/snippets/trigger-events-3-js.js
@@ -4,19 +4,9 @@ import { htmlSafe } from 'ember-string';
 export default Controller.extend({
   dropdownDisabled: true,
   checkboxClass: null,
+
   // Actions
   actions: {
-    openOnArrowDown(dropdown, e) {
-      if (e.keyCode === 40) {
-        dropdown.actions.open(e);
-      }
-    },
-
-    useTheKeyboard() {
-      alert('Use the keyboard!');
-      return false;
-    },
-
     highlightCheckboxIfDisabled() {
       if (this.get('dropdownDisabled')) {
         this.set('checkboxLabelStyle', htmlSafe('color: red'));
