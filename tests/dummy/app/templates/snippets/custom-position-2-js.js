e3410e72954f3bae3ce09df141649cd13e5a4e69
cibernox <miguel.camba@gmail.com>
2017-01-17 10:12:52 +0000

    Added missing js code

---
+++ tests/dummy/app/templates/snippets/custom-position-2-js.js
@@ -0,0 +1,16 @@
+import Controller from 'ember-controller';
+import $ from 'jquery';
+
+export default Controller.extend({
+  // Yes, same code than above. No changes needed.
+  calculatePosition(trigger, content) {
+    let { top, left, width, height } = trigger.getBoundingClientRect();
+    let { height: contentHeight } = content.getBoundingClientRect();
+    let style = {
+      left: left + width,
+      top: top + $(window).scrollTop() + (height / 2) - (contentHeight / 2)
+    };
+
+    return { style };
+  }
+});
