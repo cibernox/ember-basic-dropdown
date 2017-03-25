9267b36febe88514cb8560f9425eb660eaa6bef1
cibernox <miguel.camba@gmail.com>
2017-03-25 16:27:01 +0000

    Use async await in many tests

--- tests/.eslintrc.js
+++ tests/.eslintrc.js
@@ -1,6 +1,5 @@
 module.exports = {
   globals: {
-    clickDropdown: true,
-    andThen: true
+    clickDropdown: true
   }
 };
--- tests/acceptance/helpers-test.js
+++ tests/acceptance/helpers-test.js
@@ -4,30 +4,22 @@ import { find, visit } from 'ember-native-dom-helpers';
 
 moduleForAcceptance('Acceptance | helpers | clickDropdown');
 
-test('`clickDropdown` simulates a click in the dropdown WITHIN the given selector', function(assert) {
-  visit('/helpers-testing');
+test('`clickDropdown` simulates a click in the dropdown WITHIN the given selector', async function(assert) {
+  await visit('/helpers-testing');
 
-  andThen(function() {
-    assert.notOk(find('.ember-basic-dropdown-content'), 'There is no dropdown open');
-    clickDropdown('.dropdown-1-wrapper');
-  });
+  assert.notOk(find('.ember-basic-dropdown-content'), 'There is no dropdown open');
+  await clickDropdown('.dropdown-1-wrapper');
 
-  andThen(function() {
-    assert.ok(find('.ember-basic-dropdown-content'), 'There is a dropdown open');
-    assert.equal(find('.ember-basic-dropdown-content').textContent.trim(), 'Hello world 1!', 'There it is the expected one');
-  });
+  assert.ok(find('.ember-basic-dropdown-content'), 'There is a dropdown open');
+  assert.equal(find('.ember-basic-dropdown-content').textContent.trim(), 'Hello world 1!', 'There it is the expected one');
 });
 
-test('`clickDropdown` simulates a click in the dropdown WITH the given selector', function(assert) {
-  visit('/helpers-testing');
+test('`clickDropdown` simulates a click in the dropdown WITH the given selector', async function(assert) {
+  await visit('/helpers-testing');
 
-  andThen(function() {
-    assert.notOk(find('.ember-basic-dropdown-content'), 'There is no dropdown open');
-    clickDropdown('.dropdown-1');
-  });
+  assert.notOk(find('.ember-basic-dropdown-content'), 'There is no dropdown open');
+  await clickDropdown('.dropdown-1');
 
-  andThen(function() {
-    assert.ok(find('.ember-basic-dropdown-content'), 'There is a dropdown open');
-    assert.equal(find('.ember-basic-dropdown-content').textContent.trim(), 'Hello world 1!', 'There it is the expected one');
-  });
+  assert.ok(find('.ember-basic-dropdown-content'), 'There is a dropdown open');
+  assert.equal(find('.ember-basic-dropdown-content').textContent.trim(), 'Hello world 1!', 'There it is the expected one');
 });
