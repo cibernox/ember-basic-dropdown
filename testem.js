584832e9b670e6ddfa673f8dc9b033c2f9c5a8cd
cibernox <miguel.camba@gmail.com>
2017-04-04 22:43:08 +0100

    Try starting a screen

--- testem.js
+++ testem.js
@@ -3,21 +3,15 @@ module.exports = {
   "test_page": "tests/index.html?hidepassed",
   "disable_watching": true,
   "launch_in_ci": [
-<<<<<<< HEAD
-=======
-    // "PhantomJS",
->>>>>>> a77a18c... Remove phantomjs
     "Chrome"
   ],
   "launch_in_dev": [
-    // "PhantomJS",
     "Chrome"
   ],
   "browser_args": {
     "Chrome": [
       "--touch-events",
-      "--headless",
-      "--disable-gpu"
+      "--headless"
     ]
   }
 };
