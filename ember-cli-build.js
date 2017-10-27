c99c95d6b3a31f3f4ef1d28c9c01e7ca8a78b184
cibernox <miguel.camba@gmail.com>
2017-10-27 19:59:35 +0200

    Remove Ember pagefront

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -10,7 +10,7 @@ module.exports = function(defaults) {
   };
 
   if (project.findAddonByName('ember-native-dom-event-dispatcher')) {
-    options.vendorFiles = { 'jquery.js': null };
+    options.vendorFiles = { 'jquery.js': null, 'app-shims.js': null };
   }
 
   let app = new EmberAddon(defaults, options);
