06292e7027e4b16efe1ae786248255c04cfda5ab
cibernox <miguel.camba@gmail.com>
2018-01-28 23:07:02 +0100

    Update fastboot even more

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -9,7 +9,7 @@ module.exports = function(environment) {
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
-        'ember-glimmer-remove-application-template-wrapper': false
+        'ember-glimmer-remove-application-template-wrapper': true
       },
       EXTEND_PROTOTYPES: {
         // Prevent Ember Data from overriding Date.parse.
