33c7e2b45114f658a21e5ecc933bc455329a2416
Miguel Camba <miguel.camba@gmail.com>
2019-01-23 00:46:47 +0100

    Update to 3.7

--- config/ember-try.js
+++ config/ember-try.js
@@ -12,20 +12,19 @@ module.exports = function() {
       useYarn: true,
       scenarios: [
         {
-          name: 'ember-lts-2.16',
-          npm: {
-            devDependencies: {
-              'ember-source': '~2.16.0',
-              'ember-native-dom-event-dispatcher': '~0.6.4'
-            }
-          }
-        },
-        {
           name: 'ember-lts-2.18',
           npm: {
             devDependencies: {
               'ember-source': '~2.18.0',
               'ember-native-dom-event-dispatcher': '~0.6.4'
+            }
+          }
+        },
+        {
+          name: 'ember-lts-3.4',
+          npm: {
+            devDependencies: {
+              'ember-source': '~3.4.0'
             }
           }
         },
@@ -51,6 +50,16 @@ module.exports = function() {
             devDependencies: {
               'ember-source': urls[2]
             }
+          }
+        },
+        // The default `.travis.yml` runs this scenario via `npm test`,
+        // not via `ember try`. It's still included here so that running
+        // `ember try:each` manually or from a customized CI config will run it
+        // along with all the other scenarios.
+        {
+          name: 'ember-default',
+          npm: {
+            devDependencies: {}
           }
         },
         {
