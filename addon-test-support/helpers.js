07ce116c15830b8a526c24a030a49fcd771d1b59
Miguel Camba <miguel.camba@gmail.com>
2019-01-25 20:23:45 +0100

    Export only two test helpers

--- addon-test-support/helpers.js
+++ addon-test-support/helpers.js
@@ -1,18 +1,4 @@
-import { registerAsyncHelper } from '@ember/test';
-import { run } from '@ember/runloop';
-import { merge } from '@ember/polyfills';
-import { click, settled } from '@ember/test-helpers';
-import { deprecate } from '@ember/debug';
-
-
-export function nativeTap(selector, options = {}) {
-  let touchStartEvent = new window.Event('touchstart', { bubbles: true, cancelable: true, view: window });
-  Object.keys(options).forEach(key => touchStartEvent[key] = options[key]);
-  run(() => document.querySelector(selector).dispatchEvent(touchStartEvent));
-  let touchEndEvent = new window.Event('touchend', { bubbles: true, cancelable: true, view: window });
-  Object.keys(options).forEach(key => touchEndEvent[key] = options[key]);
-  run(() => document.querySelector(selector).dispatchEvent(touchEndEvent));
-}
+import { click, tap } from '@ember/test-helpers';
 
 export function clickTrigger(scope, options = {}) {
   let selector = '.ember-basic-dropdown-trigger';
@@ -24,8 +10,7 @@ export function clickTrigger(scope, options = {}) {
       selector = scope + ' ' + selector;
     }
   }
-  click(selector, options);
-  return settled();
+  return click(selector, options);
 }
 
 export function tapTrigger(scope, options = {}) {
@@ -33,20 +18,5 @@ export function tapTrigger(scope, options = {}) {
   if (scope) {
     selector = scope + ' ' + selector;
   }
-  nativeTap(selector, options);
-}
-
-export function fireKeydown(selector, k) {
-  let oEvent = document.createEvent('Events');
-  oEvent.initEvent('keydown', true, true);
-  merge(oEvent, {
-    view: window,
-    ctrlKey: false,
-    altKey: false,
-    shiftKey: false,
-    metaKey: false,
-    keyCode: k,
-    charCode: k
-  });
-  run(() => document.querySelector(selector).dispatchEvent(oEvent));
+  return tap(selector, options);
 }
