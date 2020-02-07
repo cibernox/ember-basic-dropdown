31dfb808dbe5729392556faad42e708a1c148bed
Miguel Camba <miguel.camba@gmail.com>
2020-02-07 18:00:28 +0100

    Update everything (#530)
    * Update many dependencies
    
    * Fix all new lint errors
    
    * Fix typo
    
    * Require node 10+
    
    * Update node in CI

--- tests/dummy/app/templates/snippets/trigger-events-4-js.js
+++ tests/dummy/app/templates/snippets/trigger-events-4-js.js
@@ -2,6 +2,7 @@ import Controller from '@ember/controller';
 import { action } from '@ember/object';
 
 export default class extends Controller {
+  parentDivClass = null;
   @action
   simulateFocusParent() {
     this.set('parentDivClass', 'input-group--focused');
