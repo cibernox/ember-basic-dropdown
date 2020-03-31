745e7e3ba4a5f56ee6385cc80c6287c904267e46
Mehul Kar <mehul.kar@gmail.com>
2020-03-31 10:36:34 -0700

    Fix animationEnabled getter to account for false value (#541)
    * Fix animationEnabled getter to account for false value
    
    If `@animationEnabled={{false}}` is passed, the getter will not use it
    
    * Use better check in case undefined prop is passed in

--- addon/components/basic-dropdown-content.js
+++ addon/components/basic-dropdown-content.js
@@ -62,7 +62,7 @@ export default @layout(templateLayout) @tagName('')class BasicDropdownContent ex
   // CPs
   @computed
   get animationEnabled() {
-    if (this._animationEnabled) {
+    if (this._animationEnabled !== 'undefined') {
       return this._animationEnabled;
     }
 
