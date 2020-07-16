53df895202c6c7f1525f53589976de81d8528ebe
Lou Greenwood <lou.greenwood@leadfeeder.com>
2020-07-16 13:18:57 +0100

    Add missing imports

--- tests/integration/components/trigger-test.js
+++ tests/integration/components/trigger-test.js
@@ -1,7 +1,14 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { hbs } from 'ember-cli-htmlbars';
-import { render, triggerEvent, triggerKeyEvent, tap, click } from '@ember/test-helpers';
+import {
+  render,
+  triggerEvent,
+  triggerKeyEvent,
+  tap,
+  click,
+  focus,
+} from "@ember/test-helpers";
 import { run } from '@ember/runloop';
 import { set } from '@ember/object'
 
