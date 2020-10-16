08c6246ca1b78cb2b784803bd510db73dd3aa282
Dmitry Krasnoukhov <dmitry@krasnoukhov.com>
2020-10-17 00:23:20 +0300

    Comment about distance

--- addon/utils/has-moved.ts
+++ addon/utils/has-moved.ts
@@ -7,6 +7,7 @@ export default function hasMoved(endEvent: TouchEvent, moveEvent?: TouchEvent): 
     return true;
   }
 
+  // Distinguish stylus scroll and tap: if touch "distance" < 5px, we consider it a tap
   let horizontalDistance = Math.abs(moveEvent.changedTouches[0].pageX - endEvent.changedTouches[0].pageX);
   let verticalDistance = Math.abs(moveEvent.changedTouches[0].pageY - endEvent.changedTouches[0].pageY);
   return horizontalDistance >= 5 || verticalDistance >= 5;
