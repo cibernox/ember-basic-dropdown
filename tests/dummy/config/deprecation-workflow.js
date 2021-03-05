49f6a2ff229969ba8417f9a625a246d02451d49e
Miguel Camba <miguel.camba@gmail.com>
2021-03-05 19:39:38 +0100

    Silence one specific warning

---
+++ tests/dummy/config/deprecation-workflow.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+self.deprecationWorkflow = self.deprecationWorkflow || {};
+self.deprecationWorkflow.config = {
+  workflow: [
+    { handler: "silence", matchId: "manager-capabilities.modifiers-3-13" },
+  ]
+};
