5f3f8de9e6b3d99b537f7bc865d0c106293cde92
Francesco Novy <francesco@fabscale.com>
2021-03-08 10:17:29 +0100

    Extract optional tag into component to avoid repetition

---
+++ addon/components/basic-dropdown-optional-tag.ts
@@ -0,0 +1,5 @@
+import Component from '@glimmer/component';
+
+interface BasicDropdownOptionalTagArgs {}
+
+export default class BasicDropdownOptionalTag extends Component<BasicDropdownOptionalTagArgs> {}
---
+++ app/templates/components/basic-dropdown-optional-tag.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-basic-dropdown/templates/components/basic-dropdown-optional-tag';
