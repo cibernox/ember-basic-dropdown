ab2b3b9ae55142bf180018af2fb30e37f22f5878
Miguel Camba <miguel.camba@gmail.com>
2021-11-26 00:25:34 +0100

    Eslint again

--- tests/dummy/app/app.js
+++ tests/dummy/app/app.js
@@ -7,6 +7,6 @@ export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
   Resolver = Resolver;
-};
+}
 
 loadInitializers(App, config.modulePrefix);
