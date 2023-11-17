29d8fc5cee28f88799b2ee4025a6997c4efe003e
Markus Sanin <markus.sanin@zeppelin-group.com>
2023-11-17 16:01:48 +0100

    Update all devDependencies * drop ember-code-example-component (replaced with own component)

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,5 +1,6 @@
 // Types for compiled templates
 declare module 'ember-basic-dropdown/templates/*' {
+  // @ts-expect-error
   import { TemplateFactory } from 'htmlbars-inline-precompile';
   const tmpl: TemplateFactory;
   export default tmpl;
