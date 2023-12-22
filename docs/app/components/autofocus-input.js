9352da54479a04960a0321a50ab93808a78227e9
Markus Sanin <markus.sanin@zeppelin-group.com>
2023-12-22 10:57:02 +0100

    Remove unused components from docs

--- docs/app/components/autofocus-input.js
+++
@@ -1,8 +0,0 @@
-import Component from '@glimmer/component';
-import { modifier } from 'ember-modifier';
-
-export default class extends Component {
-  focusInput = modifier((input) => {
-    input.focus();
-  });
-}
