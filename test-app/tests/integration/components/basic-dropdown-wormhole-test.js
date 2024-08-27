6d619d7c403ca756836b95c58a609580c53a72c9
Markus Sanin <markus.sanin@zeppelin-group.com>
2024-08-27 08:19:10 +0200

    Remove test-specific behaviour for BasicDropdownWormhole (#937)
    * use consistent BasicDropdownWormhole behaviour in tests
    
    * Fix test
    
    ---------
    
    Co-authored-by: Paul Kuruvilla <rohitpaulk@gmail.com>

--- test-app/tests/integration/components/basic-dropdown-wormhole-test.js
+++ test-app/tests/integration/components/basic-dropdown-wormhole-test.js
@@ -7,17 +7,47 @@ import config from 'test-app/config/environment';
 module('Integration | Component | basic-dropdown-wormhole', function (hooks) {
   setupRenderingTest(hooks);
 
+  hooks.beforeEach(async function () {
+    // Duplicate config to avoid mutating global config
+    this.originalConfig = JSON.parse(
+      JSON.stringify(config['ember-basic-dropdown'] || {}),
+    );
+  });
+
+  hooks.afterEach(async function () {
+    config['ember-basic-dropdown'] = this.originalConfig;
+  });
+
   test('Is present', async function (assert) {
     await render(hbs`
       <BasicDropdownWormhole />
     `);
 
-    let id = '#ember-testing';
-    if (config.APP.shadowDom) {
-      id = '#ember-basic-dropdown-wormhole';
-    }
+    assert
+      .dom('#ember-basic-dropdown-wormhole', this.element.getRootNode())
+      .exists('wormhole is present');
+  });
 
-    assert.dom(id, this.element.getRootNode()).exists('wormhole is present');
+  test('Uses custom destination from config if present', async function (assert) {
+    config['ember-basic-dropdown'] = {
+      destination: 'custom-wormhole-destination',
+    };
+
+    await render(hbs`<BasicDropdownWormhole />`);
+
+    assert
+      .dom(
+        '.ember-application  #custom-wormhole-destination',
+        this.element.getRootNode(),
+      )
+      .exists('custom destination is used');
+
+    assert
+      .dom(
+        '.ember-application #ember-basic-dropdown-wormhole',
+        this.element.getRootNode(),
+      )
+      .doesNotExist('default destination is not used');
   });
 
   test('Has class my-custom-class', async function (assert) {
