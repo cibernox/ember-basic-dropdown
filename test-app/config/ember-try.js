12e25b89e6a3b1c76485430cec627d6560041f33
Markus Sanin <markus.sanin@zeppelin-group.com>
2025-05-12 10:51:24 +0200

    Use @ember/test-helpers 5.1.0 with @ember/test-waiters 3.1.0

--- test-app/config/ember-try.js
+++ test-app/config/ember-try.js
@@ -26,6 +26,7 @@ module.exports = async function () {
         npm: {
           devDependencies: {
             '@glimmer/component': '^1.1.2',
+            '@ember/test-helpers': '5.1.0',
             '@ember/test-waiters': '^3.1.0',
             'ember-load-initializers': '^2.1.2',
             'ember-resolver': '^8.0.0',
