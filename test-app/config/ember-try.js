75ee1021429233f339f35f2d11ac171a64f7b438
Markus Sanin <markus.sanin@zeppelin-group.com>
2025-05-12 10:26:38 +0200

    Try using @ember/test-helpers 5.2.0 for 4.4

--- test-app/config/ember-try.js
+++ test-app/config/ember-try.js
@@ -26,7 +26,7 @@ module.exports = async function () {
         npm: {
           devDependencies: {
             '@glimmer/component': '^1.1.2',
-            '@ember/test-helpers': '5.2.1',
+            '@ember/test-helpers': '5.2.0',
             'ember-load-initializers': '^2.1.2',
             'ember-resolver': '^8.0.0',
             'ember-source': '~4.4.0',
