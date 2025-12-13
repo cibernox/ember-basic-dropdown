3e4ae20597f62f41dc4af630d2e99878f6e17940
Markus Sanin <markus.sanin@zeppelin-group.com>
2025-12-13 07:44:24 +0100

    Backport `@triggerHtmlTag` and add workaround to resolve button typing issue (#1033)

--- test-app/tests/integration/components/basic-dropdown-test.ts
+++ test-app/tests/integration/components/basic-dropdown-test.ts
@@ -1133,22 +1133,22 @@ module('Integration | Component | basic-dropdown', function (hooks) {
     assert.expect(12);
 
     await render(hbs`
-      <BasicDropdown as |parent|>
-        <parent.Trigger class='parent' @htmlTag="button">Trigger of the first dropdown</parent.Trigger>
+      <BasicDropdown @triggerHtmlTag="button" as |parent|>
+        <parent.Trigger class='parent'>Trigger of the first dropdown</parent.Trigger>
         <parent.Content @overlay={{true}}>
-          <BasicDropdown as |child|>
+          <BasicDropdown @triggerHtmlTag="button" as |child|>
             <p class="body-parent">
               <br>First level of the dropdpwn<br>
             </p>
-            <child.Trigger class='child' @htmlTag="button">Trigger of the second dropdown</child.Trigger>
+            <child.Trigger class='child'>Trigger of the second dropdown</child.Trigger>
             <child.Content @overlay={{true}}>
               <p class="body-child">
                 <br>Second level of the second<br>
-                <BasicDropdown as |grandchild|>
+                <BasicDropdown @triggerHtmlTag="button" as |grandchild|>
                   <p>
                     <br>Second level of the dropdpwn<br>
                   </p>
-                  <grandchild.Trigger class='grandchild' @htmlTag="button">Trigger of the Third dropdown</grandchild.Trigger>
+                  <grandchild.Trigger class='grandchild'>Trigger of the Third dropdown</grandchild.Trigger>
                   <grandchild.Content @overlay={{true}}>
                     <p class="body-grandchild">
                       <br>Third level of the third<br>
