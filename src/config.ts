424ee3f503d8dc3b2adc136e65c8be7c6814fcf1
Markus Sanin <markus.sanin@zeppelin-group.com>
2025-12-25 11:51:06 +0100

    Don't require setConfig & fix docs (#1052)

--- src/config.ts
+++ src/config.ts
@@ -1,31 +1,14 @@
 export interface Config {
   destination?: string;
-  rootElement: string;
+  rootElement?: string;
 }
 
-let _config: Config = {
-  rootElement: '',
-};
-
-let configSet = false;
+let _config: Config = {};
 
 export function setConfig(config: Config) {
-  if (!config.rootElement) {
-    throw new Error(
-      "ember-basic-dropdown: 'rootElement' is required in the config. See installation instructions for more details.",
-    );
-  }
-
   _config = config;
-  configSet = true;
 }
 
 export function getConfig(): Config {
-  if (!configSet) {
-    throw new Error(
-      'ember-basic-dropdown: setConfig was not called before accessing config. See installation instructions for more details.',
-    );
-  }
-
   return _config;
 }
