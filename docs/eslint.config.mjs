522a5057bf2826677c86acb1a1d412d890cc9bb0
Markus Sanin <markus.sanin@zeppelin-group.com>
2026-01-25 08:41:44 +0100

    Update dependencies (#1060)
    * Update @ember/addon-blueprint
    
    * Update addon dep
    
    * Update ember-cli v6.9.1...v6.10.0
    
    * Update packages in docs
    
    * Remove warp drive (not needed)
    
    * Fix format
    
    * Add missing globals for TS

--- docs/eslint.config.mjs
+++ docs/eslint.config.mjs
@@ -14,6 +14,7 @@
  */
 import globals from 'globals';
 import js from '@eslint/js';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 import ts from 'typescript-eslint';
 
@@ -23,7 +24,7 @@ import eslintConfigPrettier from 'eslint-config-prettier';
 import qunit from 'eslint-plugin-qunit';
 import n from 'eslint-plugin-n';
 
-import babelParser from '@babel/eslint-parser';
+import babelParser from '@babel/eslint-parser/experimental-worker';
 
 const parserOptions = {
   esm: {
@@ -38,25 +39,18 @@ const parserOptions = {
   },
 };
 
-export default ts.config(
+export default defineConfig([
+  globalIgnores([
+    'dist/',
+    'coverage/',
+    'app/components/snippets/*-snippet.*',
+    '!**/.*',
+  ]),
   js.configs.recommended,
   ember.configs.base,
   ember.configs.gjs,
   ember.configs.gts,
   eslintConfigPrettier,
-  /**
-   * Ignores must be in their own object
-   * https://eslint.org/docs/latest/use/configure/ignore
-   */
-  {
-    ignores: [
-      'dist/',
-      'node_modules/',
-      'coverage/',
-      'app/components/snippets/*-snippet.*',
-      '!**/.*',
-    ],
-  },
   /**
    * https://eslint.org/docs/latest/use/configure/configuration-files#configuring-linter-options
    */
@@ -85,6 +79,9 @@ export default ts.config(
     languageOptions: {
       parser: ember.parser,
       parserOptions: parserOptions.esm.ts,
+      globals: {
+        ...globals.browser,
+      },
     },
     extends: [...ts.configs.recommendedTypeChecked, ember.configs.gts],
   },
@@ -98,16 +95,7 @@ export default ts.config(
    * CJS node files
    */
   {
-    files: [
-      '**/*.cjs',
-      'config/**/*.js',
-      'testem.js',
-      'testem*.js',
-      '.prettierrc.js',
-      '.stylelintrc.js',
-      '.template-lintrc.js',
-      'ember-cli-build.js',
-    ],
+    files: ['**/*.cjs', 'config/**/*.js', 'ember-cli-build.js'],
     plugins: {
       n,
     },
@@ -138,4 +126,4 @@ export default ts.config(
       },
     },
   },
-);
+]);
