6b866c9e88417605f8bb32c760ebe5f05c3cabdb
Markus Sanin <markus.sanin@zeppelin-group.com>
2026-05-06 07:21:00 +0200

    Fix SCSS imports

--- docs/vite.config.mjs
+++ docs/vite.config.mjs
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite';
 import { extensions, classicEmberSupport, ember } from '@embroider/vite';
 import { babel } from '@rollup/plugin-babel';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        loadPaths: [path.resolve(__dirname, 'node_modules')],
+      },
+    },
+  },
   plugins: [
     classicEmberSupport(),
     ember(),
