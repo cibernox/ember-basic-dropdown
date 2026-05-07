60609cb871d0bc1c0250bd7a4a3dd23b3d339077
Markus Sanin <markus.sanin@zeppelin-group.com>
2026-05-07 07:25:38 +0200

    Add missing scss/css exports & revert workaround

--- docs/vite.config.mjs
+++ docs/vite.config.mjs
@@ -1,19 +1,8 @@
 import { defineConfig } from 'vite';
 import { extensions, classicEmberSupport, ember } from '@embroider/vite';
 import { babel } from '@rollup/plugin-babel';
-import { fileURLToPath } from 'url';
-import path from 'path';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  css: {
-    preprocessorOptions: {
-      scss: {
-        loadPaths: [path.resolve(__dirname, 'node_modules')],
-      },
-    },
-  },
   plugins: [
     classicEmberSupport(),
     ember(),
